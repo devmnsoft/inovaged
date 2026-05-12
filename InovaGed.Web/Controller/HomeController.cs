477caefdc73aefea3adff8a92590e4a276d44b9c
MNSOFT <137189378+devmnsoft@users.noreply.github.com>
2026-05-12 11:47:35 -0300

    Add interactive global error and status pages for better UX

--- InovaGed.Web/Controller/HomeController.cs
+++ InovaGed.Web/Controller/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
 
 namespace InovaGed.Web.Controllers;
 
@@ -6,4 +7,20 @@ public  class HomeController : Controller
 {
     public IActionResult Index()
         => RedirectToAction("Index", "Ged"); // manda direto pro GED
+
+    [Route("Home/Error")]
+    public IActionResult Error()
+    {
+        var feature = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+        ViewBag.RequestId = HttpContext.TraceIdentifier;
+        ViewBag.ErrorPath = feature?.Path;
+        return View();
+    }
+
+    [Route("Home/Status/{statusCode:int}")]
+    public IActionResult Status(int statusCode)
+    {
+        ViewBag.StatusCode = statusCode;
+        return View("Status");
+    }
 }
