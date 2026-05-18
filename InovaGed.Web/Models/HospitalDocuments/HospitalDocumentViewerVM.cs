771d9855262fecbddc3392332b29c8ea48733278
marcelo <marcelo.nascimento@cellent.com.br>
2026-05-18 16:44:59 -0300

    ajuste

---
+++ InovaGed.Web/Models/HospitalDocuments/HospitalDocumentViewerVM.cs
@@ -0,0 +1,22 @@
+﻿namespace InovaGed.Web.Models.HospitalDocuments;
+
+public sealed class HospitalDocumentViewerVM
+{
+    public Guid DocumentId { get; set; }
+    public Guid VersionId { get; set; }
+
+    public string Code { get; set; } = "";
+    public string Title { get; set; } = "";
+    public string FileName { get; set; } = "";
+    public string ContentType { get; set; } = "";
+    public string TypeName { get; set; } = "";
+
+    public long SizeBytes { get; set; }
+    public string SizeFormatted { get; set; } = "";
+
+    public DateTime CreatedAt { get; set; }
+
+    public string OcrText { get; set; } = "";
+    public string PreviewUrl { get; set; } = "";
+    public string OcrUrl { get; set; } = "";
+}
