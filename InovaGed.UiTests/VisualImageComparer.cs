55f3d27fc736b7f5c8e68bb20c3c8871db2f16cc
marcelo <marcelo.nascimento@cellent.com.br>
2026-07-30 19:47:03 -0300

    ajuste

--- InovaGed.UiTests/VisualImageComparer.cs
+++ InovaGed.UiTests/VisualImageComparer.cs
@@ -1,5 +1,6 @@
 using System.Security.Cryptography;
 using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Advanced;
 using SixLabors.ImageSharp.PixelFormats;
 
 namespace InovaGed.UiTests;
--- InovaGed.UiTests/VisualImageComparerTests.cs
+++ InovaGed.UiTests/VisualImageComparerTests.cs
@@ -1,5 +1,6 @@
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
+using Xunit;
 
 namespace InovaGed.UiTests;
 
