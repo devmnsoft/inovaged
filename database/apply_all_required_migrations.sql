a34b2aa4d4c598166f7f7f8ff2259a36cd41b554
MNSOFT <137189378+devmnsoft@users.noreply.github.com>
2026-08-19 20:12:00 -0300

    feat: add contract fiscalization and monthly acceptance

--- database/apply_all_required_migrations.sql
+++ database/apply_all_required_migrations.sql
@@ -3015,3 +3015,4 @@ where coalesce(reg_status, 'A') = 'A';
 
 \ir migrations/2026_08_19_executive_reports_performance.sql
 \ir migrations/2026_08_label_tracking_inventory.sql
+\ir migrations/2026_08_contract_fiscalization_acceptance.sql
